fbd061d891f779086e89ab0ceabc265ddbc62789
vprahul2008 <73024199+vprahul2008@users.noreply.github.com>
2020-12-12 16:59:43 +0530

    Add files via upload
    angry birds

---
+++ sketch.js
@@ -0,0 +1,50 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,ground,pig1,log1;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground(600,height,1200,20);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1= new Pig(810,350);
+    log1= new Log(810,260,300,PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig2= new Pig(810,220);
+    log2= new Log(810,180,300,PI/2);
+
+    bird= new Bird(100,100)
+        
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(log1.body.angle);
+
+    ground.display();
+
+    box1.display();
+    box2.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig2.display();
+    log2.display();
+
+    bird.display();
+}
